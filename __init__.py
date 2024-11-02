d718524201a26e88401a64d68cd4c882d51f8fc3
LaszloAbrok <laszlopatrikabrok@gmail.com>
2024-11-02 12:28:51 +0100

    debug support in progress

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from . import main
+# from data import DisgenetClient, DisgenetDataModule
+# from model import Model
+# if __name__ == "__main__":
+#    main()
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+
 from model import Model
 from data import DisgenetClient, DisgenetDataModule
 
+
 def main():
     disgenet_api_key = os.getenv("DISGENET_API_KEY")
-    #example_value = os.getenv("EXAMPLE_KEY")
+    # example_value = os.getenv("EXAMPLE_KEY")
 
-    if(disgenet_api_key == None):
+    if disgenet_api_key == None:
         print("[ERROR] Missing DISGENET API key.")
         return
 
@@ -14,8 +16,9 @@ def main():
         print("[LOG] Disgenet data not found. Preparing to create data...")
         disgenet_client = DisgenetClient(disgenet_api_key)
         disgenet_client.create_csv_file()
-    
+
     datamodule = DisgenetDataModule()
     print("[LOG] Finished successfully")
+
 
 main()
