32fd869af77a90a6e139052bad2c744d1bba7975
LaszloAbrok <laszlopatrikabrok@gmail.com>
2024-12-08 17:03:11 +0100

    basic hyperparameter opt

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ from model import HeteroVGAE
 from data import DisgenetClient, DisgenetDataModule, Dataset
 from sklearn.metrics import roc_auc_score, average_precision_score
 from sklearn.metrics import confusion_matrix
+import optuna
 
 
 def prepare_data_csv():
     if not os.path.exists("dga_data.csv"):
         print("[INFO] Disgenet data not found. Preparing to create data...")
-        
+
         disgenet_api_key = os.getenv("DISGENET_API_KEY")
 
         if disgenet_api_key == None:
             raise Exception("[ERROR] Missing DISGENET API key.")
-    
+
         disgenet_client = DisgenetClient(disgenet_api_key)
         disgenet_client.create_csv_file()
-        
+
         print("[INFO] Finished successfully.")
-        
+
     else:
         print("[INFO] Disgenet data found. Skipping data creation.")
 
@@ -28,48 +29,19 @@ def initialize_data(datamodule: DisgenetDataModule, dataset: Dataset):
     data = getattr(datamodule, dataset.value, None)
     if data is None:
         raise ValueError(f"Invalid dataset_type '{dataset}'.")
-    
-    node_features = {
-        "disease": data["disease"].x, 
-        "gene": data["gene"].x
-    }
+
+    node_features = {"disease": data["disease"].x, "gene": data["gene"].x}
     node_features["disease"] = node_features["disease"].view(-1, 1)
 
     edge_features = {
         ("disease", "to", "gene"): data["disease", "to", "gene"].edge_index,
         ("gene", "rev_to", "disease"): data["gene", "rev_to", "disease"].edge_index,
     }
-    
+
     return node_features, edge_features
 
 
-def initialize_model(learning_rate, edge_weight):
-    model = HeteroVGAE(in_channels_disease = 1, in_channels_gene = 2, out_channels = 1)
-    optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
-    criterion = torch.nn.BCEWithLogitsLoss(weight = edge_weight)
-    
-    return model, optimizer, criterion
-
-def evaluate(model, x_node, x_edge, y_truth):
-    model.eval()
-    output = model(x_node, x_edge)
-    prediction = torch.sigmoid(output)
-    
-    y_true = y_truth.flatten().numpy()
-    y_scores = prediction.flatten().numpy()
-    
-    roc_auc = roc_auc_score(y_true, y_scores)
-    pr_auc = average_precision_score(y_true, y_scores)
-    cm = confusion_matrix(y_true, y_scores > 0.5)
-    
-    print(f"[EVAL] ROC AUC: {roc_auc:.6f}")
-    print(f"[EVAL] PR AUC:  {pr_auc:.6f}")
-    print(cm)
-    
-    model.train()
-
-
-def main():
+def objective(trial, datamodule: DisgenetDataModule):
     prepare_data_csv()
     datamodule = DisgenetDataModule()
     datamodule.prepare_data()
@@ -77,7 +49,22 @@ def main():
     x_train_node, x_train_edge = initialize_data(datamodule, Dataset.TRAIN)
     x_val_node, x_val_edge = initialize_data(datamodule, Dataset.VAL)
     x_test_node, x_test_edge = initialize_data(datamodule, Dataset.TEST)
-    model, optimizer, criterion = initialize_model(learning_rate = 0.001, edge_weight = datamodule.weight)
+    edge_weight = datamodule.weight
+
+    learning_rate = trial.suggest_float("learning", 1e-5, 1e-1, log=False)
+    heterovgae_hidden_channels = trial.suggest_int("heterovgae_hidden_channels", 8, 32)
+    encoder_hidden_channels = trial.suggest_int("encoder_hidden_channels", 8, 32)
+    optimizier = trial.suggest_categorical("optimizer", ["Adam", "SGD"])
+
+    model, optimizer, criterion = initialize_model(
+        learning_rate,
+        heterovgae_hidden_channels,
+        encoder_hidden_channels,
+        optimizier,
+        edge_weight,
+    )
+
+    best_accuracy = 0.0
 
     for epoch in range(1, 200):
         optimizer.zero_grad()
@@ -86,14 +73,96 @@ def main():
         print(f"[TRAIN] Epoch {epoch}: {loss.item()}")
         loss.backward()
         optimizer.step()
-        
+
         if epoch % 10 == 0:
             with torch.no_grad():
-                evaluate(model, x_val_node, x_val_edge, y_truth)
-    
-    with torch.no_grad():
-        print("[INFO] Final test results")
-        evaluate(model, x_test_node, x_test_edge, y_truth)
+                accuracy = evaluate(model, x_val_node, x_val_edge, y_truth)
+                best_accuracy = max(best_accuracy, accuracy)
+
+    return best_accuracy
+
+
+def optimize_hyperparameters(datamodule: DisgenetDataModule):
+    study = optuna.create_study(direction="maximize")
+    study.optimize(lambda trial: objective(trial, datamodule), n_trials=5)
+    print(study.best_params)
+    return study.best_params
+
+
+def initialize_model(
+    learning_rate,
+    heterovgae_hidden_channels_param,
+    encoder_hidden_channels_param,
+    optimizier,
+    edge_weight,
+):
+    model = HeteroVGAE(
+        in_channels_disease=1,
+        in_channels_gene=2,
+        out_channels=1,
+        heterovgae_hidden_channels=16,  # can be optimized
+        encoder_hidden_channels=16,  # can be optimized
+    )
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = torch.nn.BCEWithLogitsLoss(weight=edge_weight)
+
+    return model, optimizer, criterion
+
+
+def evaluate(model, x_node, x_edge, y_truth):
+    model.eval()
+    output = model(x_node, x_edge)
+    prediction = torch.sigmoid(output)
+
+    y_true = y_truth.flatten().numpy()
+    y_scores = prediction.flatten().numpy()
+
+    roc_auc = roc_auc_score(y_true, y_scores)
+    pr_auc = average_precision_score(y_true, y_scores)
+    cm = confusion_matrix(y_true, y_scores > 0.5)
+
+    print(f"[EVAL] ROC AUC: {roc_auc:.6f}")
+    print(f"[EVAL] PR AUC:  {pr_auc:.6f}")
+    print(cm)
+
+    # Accuracy calculation based on the correct prediction
+    y_pred = (y_scores > 0.5).astype(int)
+    accuracy = (y_pred == y_true).mean()
+    print(f"[EVAL] Accuracy: {accuracy:.6f}")
+    model.train()
+    return accuracy
+
+
+def main():
+    prepare_data_csv()
+    datamodule = DisgenetDataModule()
+    datamodule.prepare_data()
+
+    optimize_hyperparameters(datamodule)
+
+    # y_truth = datamodule.truth_matrix
+    # x_train_node, x_train_edge = initialize_data(datamodule, Dataset.TRAIN)
+    # x_val_node, x_val_edge = initialize_data(datamodule, Dataset.VAL)
+    # x_test_node, x_test_edge = initialize_data(datamodule, Dataset.TEST)
+    # model, optimizer, criterion = initialize_model(
+    #     learning_rate=0.001, edge_weight=datamodule.weight
+    # )
+
+    # for epoch in range(1, 200):
+    #     optimizer.zero_grad()
+    #     output = model(x_train_node, x_train_edge)
+    #     loss = criterion(output, y_truth)
+    #     print(f"[TRAIN] Epoch {epoch}: {loss.item()}")
+    #     loss.backward()
+    #     optimizer.step()
+
+    #     if epoch % 10 == 0:
+    #         with torch.no_grad():
+    #             evaluate(model, x_val_node, x_val_edge, y_truth)
+
+    # with torch.no_grad():
+    #     print("[INFO] Final test results")
+    #     evaluate(model, x_test_node, x_test_edge, y_truth)
 
 
 main()
--- model/model.py
+++ model/model.py
@@ -3,11 +3,17 @@ from torch_geometric.nn import HeteroConv, SAGEConv, VGAE
 
 
 class Encoder(torch.nn.Module):
-    def __init__(self, in_channels_disease, in_channels_gene, out_channels):
+    def __init__(
+        self,
+        in_channels_disease,
+        in_channels_gene,
+        out_channels,
+        encoder_hidden_channels,
+    ):
         super(Encoder, self).__init__()
-        
-        hidden_channels = 64
-        
+
+        hidden_channels = encoder_hidden_channels
+
         self.conv1 = HeteroConv(
             {
                 ("disease", "to", "gene"): SAGEConv(
@@ -15,22 +21,19 @@ class Encoder(torch.nn.Module):
                     hidden_channels,
                 ),
                 ("gene", "rev_to", "disease"): SAGEConv(
-                    [in_channels_gene, in_channels_disease], 
-                    hidden_channels
+                    [in_channels_gene, in_channels_disease], hidden_channels
                 ),
             },
             aggr="sum",
         )
-        
+
         self.conv2 = HeteroConv(
             {
                 ("disease", "to", "gene"): SAGEConv(
-                    [hidden_channels, hidden_channels], 
-                    out_channels
+                    [hidden_channels, hidden_channels], out_channels
                 ),
                 ("gene", "rev_to", "disease"): SAGEConv(
-                    [hidden_channels, hidden_channels], 
-                    out_channels
+                    [hidden_channels, hidden_channels], out_channels
                 ),
             },
             aggr="sum",
@@ -48,25 +51,35 @@ class Decoder:
 
 
 class HeteroVGAE(torch.nn.Module):
-    def __init__(self, in_channels_disease, in_channels_gene, out_channels):
+    def __init__(
+        self,
+        in_channels_disease,
+        in_channels_gene,
+        out_channels,
+        heterovgae_hidden_channels,
+        encoder_hidden_channels,
+    ):
         super(HeteroVGAE, self).__init__()
-        
-        hidden = 32
-        
-        self.encoder = Encoder(in_channels_disease, in_channels_gene, hidden)
+
+        hidden_channels = heterovgae_hidden_channels
+
+        self.encoder = Encoder(
+            in_channels_disease,
+            in_channels_gene,
+            hidden_channels,
+            encoder_hidden_channels,
+        )
         self.decoder = Decoder()
 
-        self.fc_mu_disease = torch.nn.Linear(hidden, out_channels)
-        self.fc_logvar_disease = torch.nn.Linear(hidden, out_channels)
-        self.fc_mu_gene = torch.nn.Linear(hidden, out_channels)
-        self.fc_logvar_gene = torch.nn.Linear(hidden, out_channels)
+        self.fc_mu_disease = torch.nn.Linear(hidden_channels, out_channels)
+        self.fc_logvar_disease = torch.nn.Linear(hidden_channels, out_channels)
+        self.fc_mu_gene = torch.nn.Linear(hidden_channels, out_channels)
+        self.fc_logvar_gene = torch.nn.Linear(hidden_channels, out_channels)
 
         self.vgae = VGAE(self.encoder, self.decoder)
 
     def encode(self, x_dict, edge_index_dict):
-        encoded_features = self.encoder(
-            x_dict, edge_index_dict
-        )
+        encoded_features = self.encoder(x_dict, edge_index_dict)
 
         mu = {
             "disease": self.fc_mu_disease(encoded_features["disease"]),
@@ -76,7 +89,7 @@ class HeteroVGAE(torch.nn.Module):
             "disease": self.fc_logvar_disease(encoded_features["disease"]),
             "gene": self.fc_logvar_gene(encoded_features["gene"]),
         }
-        
+
         return mu, logvar
 
     def decode(self, z_disease, z_gene):
@@ -89,6 +102,6 @@ class HeteroVGAE(torch.nn.Module):
         # Reparameterize to obtain latent embeddings for genes and diseases
         gene_z = self.vgae.reparametrize(mu["gene"], logvar["gene"])
         disease_z = self.vgae.reparametrize(mu["disease"], logvar["disease"])
-        
+
         # Decode the latent embeddings to produce the output
         return self.decode(disease_z, gene_z)
